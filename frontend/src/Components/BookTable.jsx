d07883bfeb85ce532f0e4c363c3fc608a6d5b217
Gautam Singh Kunwar <gautamkunwar05@gmail.com>
2025-05-15 17:05:17 +0530

    fixes edit functionality in frontend

--- frontend/src/Components/BookTable.jsx
+++ frontend/src/Components/BookTable.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import TableRow from './TableRow';
-import SearchBox from './SearchBox';
-import EditCarModel from './EditCarModel';
+import { useEffect, useState } from 'react';
 import { FaTrashAlt } from "react-icons/fa";
 import { FiEdit } from "react-icons/fi";
+import EditCarModel from './EditBookModel';
+import SearchBox from './SearchBox';
+import TableRow from './TableRow';
 
 function BookTable() {
     const tableHeader = ["ISBN","BooKName","Author","PublishYear","Genre","Image","Action"];
     const[tableRowData,setTableRowData] = useState([]);
     const[refresh,setRefresh] = useState(false)
-     const[isOpen,setIsOpen] = useState(false);
+    const[isOpen,setIsOpen] = useState(false);
+    const [editedBook, setEditedBook] = useState({})
 
     useEffect(()=>{
         const fetchData = async()=>{
@@ -33,14 +34,15 @@ function BookTable() {
             setRefresh(prev=>!prev)
         }
         try {
-            const response = await axios.delete(`/api/admin/delete/${bookId}`);
+            await axios.delete(`/api/admin/delete/${bookId}`);
         } catch (error) {
             console.error("Something went wrong")
         }
     }
 
-    function handleEditClick(){
+    function handleEditClick(row){
         setIsOpen(true)
+        setEditedBook(row)
     }
     return (
         <div className='bg-green-50 h-[88vh]'>
@@ -66,13 +68,13 @@ function BookTable() {
                     td4 = {row.publishYear}
                     td6 = {row.genre}
                     td7 = {row.image}
-                    td8={<span className="flex gap-4 text-lg "><button className='cursor-pointer' onClick={()=>handleEditClick()}><FiEdit /></button>
+                    td8={<span className="flex gap-4 text-lg "><button className='cursor-pointer' onClick={()=>handleEditClick(row)}><FiEdit /></button>
                     <button className='cursor-pointer' onClick={()=>handleDelClick(row)}><FaTrashAlt /></button></span>}
                     />
                 ))}
             </tbody>
         </table>
-        {isOpen && <EditCarModel setIsOpen={setIsOpen}/>}
+        {isOpen && <EditCarModel editedBook={editedBook} setIsOpen={setIsOpen}/>}
     </div>
     )
 }
--- frontend/src/Components/EditBookModel.jsx
+++ frontend/src/Components/EditBookModel.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
-
-export default function EditCarModel({setIsOpen}) {
+export default function EditBookModel({editedBook , setIsOpen}) {
+    console.log(editedBook)
     function handleClose(){
         setIsOpen(false)
     }
-
     return (
         <>
-
-            <div className="min-h-screen flex items-center justify-center backdrop-blur-sm  absolute top-0 w-full left-0 z-100">
+            <div className="min-h-screen flex items-center justify-center backdrop-blur-[5px]  absolute top-0 w-full left-0 z-100">
                 <form className="space-y-1 p-7 bg-green-50">
                     <div className="flex flex-col md:flex-row gap-4">
                         <div className="w-full">
@@ -18,6 +15,7 @@ export default function EditCarModel({setIsOpen}) {
                                 id="bookName"
                                 name="bookName"
                                 placeholder="Enter book name"
+                                value={editedBook.bookName}
                                 className="w-full p-2 border border-gray-300 rounded-md bg-white outline-0"
                             />
                         </div>
@@ -28,6 +26,7 @@ export default function EditCarModel({setIsOpen}) {
                                 id="author"
                                 name="author"
                                 placeholder="Enter author name"
+                                value={editedBook.author}
                                 className="w-full p-2 border border-gray-300 rounded-md bg-white outline-0"
                             />
                         </div>
@@ -41,6 +40,7 @@ export default function EditCarModel({setIsOpen}) {
                                 id="isbn"
                                 name="isbn"
                                 placeholder="Enter ISBN"
+                                value={editedBook.isbn}
                                 className="w-full p-2 border border-gray-300 rounded-md bg-white outline-0"
                             />
                         </div>
@@ -51,6 +51,7 @@ export default function EditCarModel({setIsOpen}) {
                                 id="publishYear"
                                 name="publishYear"
                                 placeholder="Enter publication year"
+                                value={editedBook.publishYear}
                                 className="w-full p-2 border border-gray-300 rounded-md bg-white outline-0"
                             />
                         </div>
@@ -61,6 +62,7 @@ export default function EditCarModel({setIsOpen}) {
                         <select
                             id="genre"
                             name="genre"
+                            defaultValue={editedBook.genre}
                             className="w-full p-2 border border-gray-300 rounded-md bg-white"
                         >
                             <option value="">Select</option>
@@ -80,6 +82,7 @@ export default function EditCarModel({setIsOpen}) {
                             name="desc"
                             placeholder="Enter book description"
                             className="w-full p-2 border border-gray-300 rounded-md bg-white outline-0"
+                            value={editedBook.desc}
                         />
                     </div>
 
@@ -92,6 +95,7 @@ export default function EditCarModel({setIsOpen}) {
                                 name="image"
                                 accept="image/*"
                                 className="w-full p-2 border border-gray-300 rounded-md bg-white outline-0"
+                                
                             />
                         </div>
                         <div className="w-full">
